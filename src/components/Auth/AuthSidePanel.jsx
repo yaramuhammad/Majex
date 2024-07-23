90f77c36116b1cfcfb32fcea376744916b8dc103
Walaa soudy <11410120201218@stud.cu.edu.eg>
2024-07-23 17:56:55 +0300

    add images in src

--- src/components/Auth/AuthSidePanel.jsx
+++ src/components/Auth/AuthSidePanel.jsx
@@ -1,14 +1,22 @@
+import React from 'react';
+import image1 from '../../assets/images 1.png';
+import logo from '../../assets/logo PNG-01 1.png';
+
 const AuthSidePanel = () => {
     return (
-        <div className="w-1/2 bg-cover border-r-2 border-black" style={{
-            backgroundImage: `url('/assets/images 1.png')`,
-            backgroundPosition: 'center'
-        }}>
-            <div className="flex justify-center items-center h-full">
-                <img src="/assets/logo PNG-01 1.png" alt="Majex International" className="w-1/2" />
+        <div className="w-1/2 border-r-2 border-black relative">
+            <div className="absolute inset-0">
+                <img 
+                    src={image1} 
+                    alt="Background" 
+                    className="w-full h-full object-cover"
+                />
+            </div>
+            <div className="relative flex justify-center items-center h-full">
+                <img src={logo} alt="Majex International" className="w-1/2" />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default AuthSidePanel
+export default AuthSidePanel;
