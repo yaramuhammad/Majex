5cbf4ec309e2b9627af6ba72095a0ad9e3191ff2
Yara <yaramuhammad762@gmail.com>
2024-08-08 21:41:22 +0300

    localization

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -6,10 +6,14 @@ import AuthSidePanel from './AuthSidePanel';
 import toast from 'react-hot-toast';
 import { ThreeDots } from 'react-loader-spinner';
 import axios from 'axios';
+import { useTranslation, getI18n } from 'react-i18next';
 
 const Login = () => {
     const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
+    const { t } = useTranslation();
+    const i18n = getI18n();
+    const currentLanguage = i18n.language;
 
     const formik = useFormik({
         initialValues: {
@@ -17,8 +21,8 @@ const Login = () => {
             password: '',
         },
         validationSchema: Yup.object({
-            email: Yup.string().email('Invalid email address').required('Email is required'),
-            password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+            email: Yup.string().email(t('Invalid email address')).required(t('Email is required')),
+            password: Yup.string().min(6, t('Password must be at least 6 characters')).required(t('Password is required')),
         }),
         onSubmit: async (values, { setErrors }) => {
             setIsLoading(true);
@@ -30,54 +34,53 @@ const Login = () => {
                 });
                 const data = response.data;
                 localStorage.setItem('token', data.token);
-                toast.success('Logged in Successfully');
+                toast.success(t('Logged in Successfully'));
                 navigate('/');
             } catch (error) {
                 if (error.response && error.response.data) {
                     setErrors(error.response.data.errors);
                 } else {
-                    toast.error('An error occurred. Please try again.');
+                    toast.error(t('An error occurred. Please try again.'));
                 }
             } finally {
                 setIsLoading(false);
             }
         },
-    
     });
 
     return (
-        <div className="flex h-screen">
+        <div className={`flex h-screen ${currentLanguage === 'ar' ? 'flex-row-reverse' : ''}`}>
             <AuthSidePanel />
             <div className="w-1/2 flex justify-center items-center bg-white">
                 <div className="w-3/4">
-                    <h2 className="text-2xl font-bold text-center mb-12">Hey, There!</h2>
-                    <div className="flex justify-evenly mt-4">
-                        <Link to="/login" className="primary-color border-b-2 primary-underline">Login</Link>
-                        <Link to="/register" className="ml-8 text-gray-600 border-b-2 border-black">Sign up</Link>
+                    <h2 className={`text-2xl font-bold text-center mb-12 ${currentLanguage === 'ar' ? 'text-right' : ''}`}>{t('Hey, There!')}</h2>
+                    <div className={`flex justify-evenly mt-4 ${currentLanguage === 'ar' ? 'flex-row-reverse' : ''}`}>
+                        <Link to="/login" className={`primary-color border-b-2 primary-underline ${currentLanguage === 'ar' ? 'ml-8' : ''}`}>{t('Login')}</Link>
+                        <Link to="/register" className={`text-gray-600 border-b-2 border-black ${currentLanguage === 'ar' ? 'ml-8' : ''}`}>{t('Sign up')}</Link>
                     </div>
                     <form onSubmit={formik.handleSubmit} className="mt-8">
                         <input
                             type="email"
-                            placeholder="Email"
+                            placeholder={t('Email')}
                             {...formik.getFieldProps('email')}
-                            className={`px-4 rounded-2xl w-full p-2 border-2 ${formik.touched.email && formik.errors.email ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
+                            className={`px-4 rounded-2xl w-full p-2 border-2 ${formik.touched.email && formik.errors.email ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none ${currentLanguage === 'ar' ? 'text-right' : ''}`}
                         />
                         {formik.touched.email && formik.errors.email ? (
-                            <div className="text-red-600 text-xs">{formik.errors.email}</div>
+                            <div className={`text-red-600 text-xs ${currentLanguage === 'ar' ? 'text-right' : ''}`}>{formik.errors.email}</div>
                         ) : null}
 
                         <input
                             type="password"
-                            placeholder="Password"
+                            placeholder={t('Password')}
                             {...formik.getFieldProps('password')}
-                            className={`px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.password && formik.errors.password ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
+                            className={`px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.password && formik.errors.password ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none ${currentLanguage === 'ar' ? 'text-right' : ''}`}
                         />
                         {formik.touched.password && formik.errors.password ? (
-                            <div className="text-red-600 text-xs">{formik.errors.password}</div>
+                            <div className={`text-red-600 text-xs ${currentLanguage === 'ar' ? 'text-right' : ''}`}>{formik.errors.password}</div>
                         ) : null}
 
-                        <div className="flex justify-between items-center mt-4">
-                            <a href="#" className="primary-color">Forgot your password?</a>
+                        <div className={`flex justify-between items-center mt-4 ${currentLanguage === 'ar' ? 'flex-row-reverse' : ''}`}>
+                            <a href="#" className={`primary-color ${currentLanguage === 'ar' ? 'text-right' : ''}`}>{t('Forgot your password?')}</a>
                         </div>
 
                         <button type="submit" className={isLoading ? "primary-bg mt-8 w-full text-white px-4 py-4 rounded-xl flex justify-center" : "primary-bg mt-8 w-full text-white px-4 py-2 rounded-xl flex justify-center"}>
@@ -87,7 +90,7 @@ const Login = () => {
                                 width="80"
                                 color="white"
                                 radius="1"
-                            /> : 'Login'}
+                            /> : t('Login')}
                         </button>
                     </form>
                 </div>
--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -6,11 +6,15 @@ import AuthSidePanel from './AuthSidePanel';
 import toast from 'react-hot-toast';
 import { ThreeDots } from 'react-loader-spinner';
 import axios from 'axios';
-import i18n from '../../i18n';
+import { useTranslation, getI18n } from 'react-i18next';
 
 const Register = () => {
     const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
+    const { t } = useTranslation();
+    const i18n = getI18n();
+    const currentLanguage = i18n.language;
+
 
     const formik = useFormik({
         initialValues: {
@@ -21,16 +25,16 @@ const Register = () => {
             mobileNumber: '',
             whatsappNumber: '',
             termsAndConditionsAgreed: false,
-            language: i18n.language || 'en', // Set initial language value
+            language: '',
         },
         validationSchema: Yup.object({
-            name: Yup.string().required('Name is required'),
-            email: Yup.string().email('Invalid email address').required('Email is required'),
-            password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
-            password2: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').required('Password Confirmation is required'),
-            mobileNumber: Yup.string().required('Mobile number is required'),
-            termsAndConditionsAgreed: Yup.boolean().oneOf([true], 'You must accept the terms and conditions').required('You must accept the terms and conditions'),
-            language: Yup.string().required('Language is required'),
+            name: Yup.string().required(t('Name is required')),
+            email: Yup.string().email(t('Invalid email address')).required(t('Email is required')),
+            password: Yup.string().min(6, t('Password must be at least 6 characters')).required(t('Password is required')),
+            password2: Yup.string().oneOf([Yup.ref('password'), null], t('Passwords must match')).required(t('Password Confirmation is required')),
+            mobileNumber: Yup.string().required(t('Mobile number is required')),
+            termsAndConditionsAgreed: Yup.boolean().oneOf([true], t('You must accept the terms and conditions')).required(t('You must accept the terms and conditions')),
+            language: Yup.string().required(t('Language is required')),
         }),
         onSubmit: async (values, { setErrors }) => {
             setIsLoading(true);
@@ -43,14 +47,14 @@ const Register = () => {
 
                 const data = response.data;
                 localStorage.setItem('token', data.token);
-                toast.success('Registered Successfully');
+                toast.success(t('Registered Successfully'));
                 navigate('/');
             } catch (error) {
                 if (error.response && error.response.data) {
                     console.log(error.response.data.errors);
                     setErrors(error.response.data.errors);
                 } else {
-                    toast.error('An error occurred. Please try again.');
+                    toast.error(t('An error occurred. Please try again.'));
                 }
             } finally {
                 setIsLoading(false);
@@ -59,110 +63,110 @@ const Register = () => {
     });
 
     return (
-        <div className="flex h-screen">
+        <div className={`flex h-screen ${currentLanguage==='ar'?'flex-row-reverse' : ''}`}>
             <AuthSidePanel />
             <div className="w-1/2 flex justify-center items-center bg-white">
                 <div className="w-3/4">
-                    <h2 className="text-2xl font-bold text-center mb-12">Hey, There!</h2>
-                    <div className="flex justify-evenly mt-4">
-                        <Link to="/login" className="text-gray-600 border-b-2 border-black">Login</Link>
-                        <Link to="/register" className="ml-8 primary-color border-b-2 primary-underline">Sign up</Link>
+                    <h2 className="text-2xl font-bold text-center mb-12">{t('Hey, There!')}</h2>
+                    <div className={`flex justify-evenly mt-4 ${currentLanguage === 'ar' ? 'flex-row-reverse' : ''}`}>
+                        <Link to="/login" className="text-gray-600 border-b-2 border-black">{t('Login')}</Link>
+                        <Link to="/register" className="ml-8 primary-color border-b-2 primary-underline">{t('Sign up')}</Link>
                     </div>
                     <form onSubmit={formik.handleSubmit} className="mt-8">
                         <input
                             type="text"
-                            placeholder="Name"
+                            placeholder={t('Name')}
                             {...formik.getFieldProps('name')}
-                            className={`px-4 rounded-2xl w-full p-2 border-2 ${formik.touched.name && formik.errors.name ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
+                            className={`${currentLanguage==='ar'?'text-right' : ''} px-4 rounded-2xl w-full p-2 border-2 ${formik.touched.name && formik.errors.name ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
                         />
                         {formik.touched.name && formik.errors.name ? (
-                            <div className="text-red-600 text-xs">{formik.errors.name}</div>
+                            <div className={`text-red-600 text-xs ${currentLanguage==='ar'?'text-right' : ''}`}>{formik.errors.name}</div>
                         ) : null}
 
                         <input
                             type="email"
-                            placeholder="Email"
+                            placeholder={t('Email')}
                             {...formik.getFieldProps('email')}
-                            className={`px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.email && formik.errors.email ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
+                            className={`${currentLanguage==='ar'?'text-right' : ''} px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.email && formik.errors.email ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
                         />
                         {formik.touched.email && formik.errors.email ? (
-                            <div className="text-red-600 text-xs">{formik.errors.email}</div>
+                            <div className={`text-red-600 text-xs ${currentLanguage==='ar'?'text-right' : ''}`}>{formik.errors.email}</div>
                         ) : null}
 
                         <input
                             type="password"
-                            placeholder="Password"
+                            placeholder={t('Password')}
                             {...formik.getFieldProps('password')}
-                            className={`px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.password && formik.errors.password ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
+                            className={`${currentLanguage==='ar'?'text-right' : ''} px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.password && formik.errors.password ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
                         />
                         {formik.touched.password && formik.errors.password ? (
-                            <div className="text-red-600 text-xs">{formik.errors.password}</div>
+                            <div className={`text-red-600 text-xs ${currentLanguage==='ar'?'text-right' : ''}`}>{formik.errors.password}</div>
                         ) : null}
 
                         <input
                             type="password"
-                            placeholder="Password Confirmation"
+                            placeholder={t('Password Confirmation')}
                             {...formik.getFieldProps('password2')}
-                            className={`px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.password2 && formik.errors.password2 ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
+                            className={`${currentLanguage==='ar'?'text-right' : ''} px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.password2 && formik.errors.password2 ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
                         />
                         {formik.touched.password2 && formik.errors.password2 ? (
-                            <div className="text-red-600 text-xs">{formik.errors.password2}</div>
+                            <div className={`text-red-600 text-xs ${currentLanguage==='ar'?'text-right' : ''}`}>{formik.errors.password2}</div>
                         ) : null}
 
                         <input
                             type="phone"
-                            placeholder="Mobile Number"
+                            placeholder={t('Mobile Number')}
                             {...formik.getFieldProps('mobileNumber')}
-                            className={`px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.mobileNumber && formik.errors.mobileNumber ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
+                            className={`${currentLanguage==='ar'?'text-right' : ''} px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.mobileNumber && formik.errors.mobileNumber ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
                         />
                         {formik.touched.mobileNumber && formik.errors.mobileNumber ? (
-                            <div className="text-red-600 text-xs">{formik.errors.mobileNumber}</div>
+                            <div className={`text-red-600 text-xs ${currentLanguage==='ar'?'text-right' : ''}`}>{formik.errors.mobileNumber}</div>
                         ) : null}
 
                         <input
                             type="phone"
-                            placeholder="Whatsapp Number"
+                            placeholder={t('Whatsapp Number')}
                             {...formik.getFieldProps('whatsappNumber')}
-                            className={`px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.whatsappNumber && formik.errors.whatsappNumber ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
+                            className={`${currentLanguage==='ar'?'text-right' : ''} px-4 rounded-2xl w-full p-2 mt-4 border-2 ${formik.touched.whatsappNumber && formik.errors.whatsappNumber ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
                         />
                         {formik.touched.whatsappNumber && formik.errors.whatsappNumber ? (
-                            <div className="text-red-600 text-xs">{formik.errors.whatsappNumber}</div>
+                            <div className={`text-red-600 text-xs ${currentLanguage==='ar'?'text-right' : ''}`}>{formik.errors.whatsappNumber}</div>
                         ) : null}
+
                         <div className="mt-4">
-                            <label htmlFor="language" className="block text-gray-600">Preferred Language</label>
                             <select
                                 id="language"
                                 {...formik.getFieldProps('language')}
-                                className={`px-4 py-2 rounded-2xl w-full border-2 ${formik.touched.language && formik.errors.language ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
+                                className={`${currentLanguage==='ar'?'text-right' : ''} px-4 py-2 rounded-2xl w-full border-2 ${formik.touched.language && formik.errors.language ? 'border-red-600' : 'border-gray-300'} focus:border-red-600 outline-none`}
                             >
-                                <option value="english">English</option>
-                                <option value="chinese">Chinese</option>
-                                <option value="arabic">Arabic</option>
+                                <option value="" disabled selected>{t('Preferred Language')}</option>
+                                <option value="english">{t('En')}</option>
+                                <option value="chinese">{t('中文')}</option>
+                                <option value="arabic">{t('Ar')}</option>
                             </select>
                             {formik.touched.language && formik.errors.language ? (
-                                <div className="text-red-600 text-xs">{formik.errors.language}</div>
+                                <div className={`text-red-600 text-xs ${currentLanguage==='ar'?'text-right' : ''}`}>{formik.errors.language}</div>
                             ) : null}
                         </div>
 
-
-                        <div className="flex justify-between mt-4">
+                        <div className={`flex justify-between mt-4 ${currentLanguage==='ar'?'flex-row-reverse' : ''}`}>
                             <label htmlFor="termsAndConditionsAgreed" className="text-gray-600">
-                                You agree to our
-                                <span className='primary-color'> terms</span> and
-                                <span className='primary-color'> conditions</span>
+                                {t('You agree to our')}
+                                <span className='primary-color'> {t('terms')}</span> {t('and')}
+                                <span className='primary-color'> {t('conditions')}</span>
                             </label>
                             <input
                                 type="checkbox"
                                 id="termsAndConditionsAgreed"
                                 {...formik.getFieldProps('termsAndConditionsAgreed')}
-                                className={`form-checkbox text-red-600 text-xs border-gray-300 rounded focus:ring-red-500 ${formik.touched.termsAndConditionsAgreed && formik.errors.termsAndConditionsAgreed ? 'border-red-600' : 'border-gray-300'}`}
+                                className={
+                                    ` form-checkbox text-red-600 text-xs border-gray-300 rounded focus:ring-red-500 ${formik.touched.termsAndConditionsAgreed && formik.errors.termsAndConditionsAgreed ? 'border-red-600' : 'border-gray-300'}`}
                             />
                         </div>
                         {formik.touched.termsAndConditionsAgreed && formik.errors.termsAndConditionsAgreed ? (
-                            <div className="text-red-600 text-xs">{formik.errors.termsAndConditionsAgreed}</div>
+                            <div className={`text-red-600 text-xs ${currentLanguage==='ar'?'text-right' : ''}`}>{formik.errors.termsAndConditionsAgreed}</div>
                         ) : null}
 
-                        
                         <button type="submit" className={isLoading ? "primary-bg mt-8 w-full text-white px-4 py-4 rounded-xl flex justify-center" : "primary-bg mt-8 w-full text-white px-4 py-2 rounded-xl flex justify-center"}>
                             {isLoading ? <ThreeDots
                                 visible={true}
@@ -170,7 +174,7 @@ const Register = () => {
                                 width="80"
                                 color="white"
                                 radius="1"
-                            /> : 'Sign up'}
+                            /> : t('Sign up')}
                         </button>
                     </form>
                 </div>
