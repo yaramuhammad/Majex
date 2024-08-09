155f57b7946e0198df1fca6f2983e73a01e5f4d8
Yara <yaramuhammad762@gmail.com>
2024-08-09 11:06:58 +0300

    Profile Localization + rtl

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import Loading from './../Loading'
+import Loading from './../Loading';
+import { useTranslation, getI18n } from 'react-i18next';
 
 function Profile() {
+  const { t } = useTranslation();
+  const i18n = getI18n();
+  const currentLanguage = i18n.language; 
+
   const [userData, setUserData] = useState({
     name: '',
     email: '',
@@ -14,7 +19,7 @@ function Profile() {
     interests: '',
   });
 
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,7 +44,7 @@ function Profile() {
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
-        setLoading(false); 
+        setLoading(false);
       }
     };
 
@@ -47,40 +52,40 @@ function Profile() {
   }, []);
 
   if (loading) {
-    return <Loading/>
+    return <Loading />;
   }
 
   return (
     <div className="grid grid-cols-5 h-screen">
-      <div className="col-span-3 h-full p-5">
-        <div className="flex items-center space-x-4 py-4">
+      <div className={`col-span-3 h-full p-5 ${currentLanguage==='ar'? 'order-2' : ''}`}>
+        <div className={`flex items-center space-x-4 py-4 ${currentLanguage==='ar'? 'flex-row-reverse space-x-reverse' : ''}`}>
           <div>
             <i className="fa-regular fa-user fa-2xl"></i>
           </div>
-          <div className="w-3/4">
+          <div>
             <span className="text-gray-800 font-bold text-3xl">{userData.name}</span>
           </div>
         </div>
         <form className="space-y-4">
           {[
-            { label: 'Name', value: userData.name, type: 'text', placeholder: 'Enter your name' },
-            { label: 'Email', value: userData.email, type: 'email', placeholder: 'Enter your email' },
-            { label: 'Password', value: userData.password, type: 'password', placeholder: 'Enter your password' },
-            { label: 'Mobile Number', value: userData.mobileNumber, type: 'tel', placeholder: 'Enter your mobile number' },
-            { label: 'WhatsApp Number', value: userData.whatsappNumber, type: 'tel', placeholder: 'Enter your WhatsApp number' },
-            { label: 'Interests', value: userData.interests, type: 'text', placeholder: 'Enter your interests' }
+            { label: t('name'), value: userData.name, type: 'text', placeholder: t('enterName') },
+            { label: t('email'), value: userData.email, type: 'email', placeholder: t('enterEmail') },
+            { label: t('password'), value: userData.password, type: 'password', placeholder: t('enterPassword') },
+            { label: t('mobileNumber'), value: userData.mobileNumber, type: 'tel', placeholder: t('enterMobileNumber') },
+            { label: t('whatsappNumber'), value: userData.whatsappNumber, type: 'tel', placeholder: t('enterWhatsappNumber') },
+            { label: t('interests'), value: userData.interests, type: 'text', placeholder: t('enterInterests') }
           ].map((field, index) => (
             <div key={index} className="flex flex-col space-y-2">
-              <label className="text-custom-red font-semibold">{field.label}</label>
+              <label className={`text-custom-red font-semibold ${currentLanguage==='ar'? ' text-right' : ''}`}>{field.label}</label>
               <div className="relative">
                 <input
                   type={field.type}
                   value={field.value}
                   placeholder={field.placeholder}
-                  className="border border-gray-300 rounded-lg py-3 w-full pr-10 bg-gray-100"
+                  className={`border border-gray-300 rounded-lg py-3 w-full bg-gray-100  ${currentLanguage==='ar'? ' text-right pl-10' : 'pr-10'}`}
                   readOnly
                 />
-                <Link to="/edit" className="absolute right-3 top-1/2 transform -translate-y-1/2">
+                <Link to="/edit" className={`absolute right-3 top-1/2 transform -translate-y-1/2  ${currentLanguage==='ar'? ' left-3' : ''}`}>
                   <i className="fa-regular fa-pen-to-square w-6 h-6 cursor-pointer primary-color fa-xl"></i>
                 </Link>
               </div>
@@ -89,24 +94,24 @@ function Profile() {
         </form>
       </div>
 
-      <div className="col-span-2 h-full p-5">
+      <div className="col-span-2 h-full px-5">
         <div className='mt-10'></div>
         <div className='p-10'></div>
 
-        <div className='bg-[#f3f3f3] flex px-5 py-3 rounded-lg'>
+        <div className={`bg-[#f3f3f3] flex px-5 py-3 rounded-lg  ${currentLanguage==='ar'? ' flex-row-reverse' : ''}`}>
           <div className="w-1/2 pr-2">
             <div className="flex flex-col items-center">
               <img src={require('./../../assets/crown.png')} alt="package-logo" className="mb-2" />
               <div className="text-left">
-                <p className="text-custom-red font-bold">VIP PACKAGES</p>
+                <p className="text-custom-red font-bold">{t('vipPackages')}</p>
                 <p className="text-gray-800 font-bold">1875.00 SAR</p>
               </div>
             </div>
           </div>
           <div className="w-1/2 pl-2 pt-5 mt-2">
             <div className="flex flex-col items-center">
-              <p className="text-gray-800 font-bold">Subscription Plan</p>
-              <p className="text-gray-800">3 months left</p>
+              <p className="text-gray-800 font-bold">{t('subscriptionPlan')}</p>
+              <p className="text-gray-800">{t('monthsLeft')}</p>
             </div>
           </div>
         </div>
